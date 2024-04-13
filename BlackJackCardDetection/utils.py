c8bc6601483f410c3b71977f5a051c91b3ccf728
0xBitBuster <ScharlT.coding@gmail.com>
2024-04-13 16:25:37 +0200

    Refactored Blackjack Counter

--- BlackJackCardDetection/utils.py
+++ BlackJackCardDetection/utils.py
@@ -1,3 +1,5 @@
+import cv2 as cv
+
 def calculate_best_count(cards):
     card_values = {
         'A': 11,  # Ace
@@ -12,35 +14,24 @@ def calculate_best_count(cards):
     for card in cards:
         value, suit = card[:-1], card[-1]
 
-        # Check if the value is a special card (Ace, King, Queen, Jack)
         if value in card_values:
             count += card_values[value]
             if value == 'A':
                 has_ace = True
         else:
-            # Convert the value to an integer
             value = int(value)
             count += value
 
-        # If the count exceeds 21 and there's an Ace, subtract 10 to make it 1
         if count > 21 and has_ace:
             count -= 10
-            has_ace = False  # Reset the flag since we've adjusted the Ace's value
+            has_ace = False
 
     return count
 
 
-def merge_bboxes(existing_bbox, new_bbox):
-    top = min(new_bbox['y'] - new_bbox['height'] / 2, existing_bbox['y'] - existing_bbox['height'] / 2)
-    bottom = max(new_bbox['y'] + new_bbox['height'] / 2, existing_bbox['y'] + existing_bbox['height'] / 2)
-    left = min(new_bbox['x'] - new_bbox['width'] / 2, existing_bbox['x'] - existing_bbox['width'] / 2)
-    right = max(new_bbox['x'] + new_bbox['width'] / 2, existing_bbox['x'] + existing_bbox['width'] / 2)
-
-    merged_bbox = {
-        'x': (left + right) / 2,
-        'y': (top + bottom) / 2,
-        'width': right - left,
-        'height': bottom - top
-    }
-
-    return merged_bbox
+def draw_text_with_background(frame, text, position, font=cv.FONT_HERSHEY_SIMPLEX, font_scale=1, font_thickness=1, text_color=(255, 255, 255), background_color=(0, 0, 0)):
+    text_size, _ = cv.getTextSize(text, font, font_scale, font_thickness)
+    background_rect_x = position[0] - 2
+    background_rect_y = position[1] - text_size[1] - 2
+    cv.rectangle(frame, (background_rect_x, background_rect_y), (position[0] + text_size[0] + 2, position[1] + 2), background_color, -1)
+    cv.putText(frame, text, position, font, font_scale, text_color, font_thickness)
